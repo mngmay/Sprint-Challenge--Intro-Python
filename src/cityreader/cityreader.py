d5bbcc4df506e2542118d94efc879514d668f855
May Ng <mngmay@gmail.com>
2019-12-06 09:10:20 -0800

    completed cityreader.py mvp. Actually done with mvp now :D

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -2,11 +2,17 @@
 # fields for name, lat and lon (representing latitude and longitude).
 
 
+import csv
+
+
 class City:
     def __init__(self, name, lat, lon):
         self.name = name
         self.lat = lat
         self.lon = lon
+
+    def __str__(self):
+        return f"({self.name}, {self.lat}, {self.lon})"
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -23,11 +29,22 @@ class City:
 # should not be loaded into a City object.
 cities = []
 
+print(cities)
+
+# with open('cities.csv') as csvfile:
+#   print(csvfile.read())
+
 
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the
-  # `cities` list
+    # TODO Implement the functionality to read from the 'cities.csv' file
+    # For each city record, create a new City instance and add it to the
+    # `cities` list
+    data = list(csv.reader(open('cities.csv')))
+    print(data)
+
+    for city in data:
+        new_city = City(city[0], city[3], city[4])
+        cities.append(new_city)
 
     return cities
 
@@ -71,7 +88,7 @@ for c in cities:
 
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
+    # within will hold the cities that fall within the specified region
     within = []
 
     # TODO Ensure that the lat and lon valuse are all floats
